d56634f541543c1b6bb2d2bcef668b998e7f1d4e
Rob Rakaric <rakaricr@gmail.com>
2022-10-31 09:33:37 -0700

    externalized mtls ca config for service mesh

--- linkerd-helm-k8s/main.tf
+++ linkerd-helm-k8s/main.tf
@@ -1,64 +1,14 @@
 terraform {
   required_providers {
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4"
-    }
     helm = {
       source = "hashicorp/helm"
       version = "~> 2"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2"
+    }
   }
-}
-
-resource "tls_private_key" "mtls-root-ca" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
-}
-
-resource "tls_self_signed_cert" "mtls-root-ca" {
-  is_ca_certificate = true
-  private_key_pem   = tls_private_key.mtls-root-ca.private_key_pem
-  subject {
-    common_name = "root.linkerd.cluster.local"
-  }
-  dns_names = ["root.linkerd.cluster.local"]
-  allowed_uses = [
-    "key_encipherment",
-    "digital_signature",
-    "server_auth",
-    "client_auth",
-    "cert_signing"
-  ]
-  validity_period_hours = 87600
-}
-
-resource "tls_private_key" "mtls-intermediate-ca" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
-}
-
-resource "tls_cert_request" "mtls-intermediate-ca" {
-  private_key_pem = tls_private_key.mtls-intermediate-ca.private_key_pem
-  subject {
-    common_name = "identity.linkerd.cluster.local"
-  }
-  dns_names = ["identity.linkerd.cluster.local"]
-}
-
-resource "tls_locally_signed_cert" "mtls-intermediate-ca" {
-  is_ca_certificate  = true
-  cert_request_pem   = tls_cert_request.mtls-intermediate-ca.cert_request_pem
-  ca_private_key_pem = tls_private_key.mtls-root-ca.private_key_pem
-  ca_cert_pem        = tls_self_signed_cert.mtls-root-ca.cert_pem
-  allowed_uses = [
-    "key_encipherment",
-    "digital_signature",
-    "server_auth",
-    "client_auth",
-    "cert_signing"
-  ]
-  validity_period_hours = 8760
 }
 
 resource "kubernetes_namespace" "linkerd" {
@@ -67,6 +17,9 @@ resource "kubernetes_namespace" "linkerd" {
   }
 }
 
+# Install Linkerd Container Network Interface (CNI) Plugin
+# https://linkerd.io/2.12/features/cni/#using-helm
+
 resource "helm_release" "linkerd2-cni" {
   name       = "linkerd2-cni"
   namespace = var.linkerd_k8s_namespace
@@ -74,6 +27,9 @@ resource "helm_release" "linkerd2-cni" {
   chart      = "linkerd2-cni"
   version    = var.linkerd_cni_version
 }
+
+# Install Linkerd CRDs and Control Plane
+# https://linkerd.io/2.12/tasks/install-helm/#helm-install-procedure
 
 resource "helm_release" "linkerd-crds" {
   depends_on = [
@@ -91,19 +47,19 @@ resource "helm_release" "linkerd-crds" {
   }
   set {
     name  = "identityTrustAnchorsPEM"
-    value = tls_self_signed_cert.mtls-root-ca.cert_pem
+    value = var.mtls_root_ca_cert
   }
   set {
     name  = "identity.issuer.tls.crtPEM"
-    value = tls_locally_signed_cert.mtls-intermediate-ca.cert_pem
+    value = var.mtls_intermediate_ca_cert
   }
   set {
     name  = "identity.issuer.tls.keyPEM"
-    value = tls_private_key.mtls-intermediate-ca.private_key_pem
+    value = var.mtls_intermediate_ca_private_key
   }
   set {
     name  = "identity.issuer.crtExpiry"
-    value = tls_locally_signed_cert.mtls-intermediate-ca.validity_end_time
+    value = var.mtls_cert_expiry
   }
 }
 
@@ -127,18 +83,18 @@ resource "helm_release" "linkerd-control-plane" {
   }
   set {
     name  = "identityTrustAnchorsPEM"
-    value = tls_self_signed_cert.mtls-root-ca.cert_pem
+    value = var.mtls_root_ca_cert
   }
   set {
     name  = "identity.issuer.tls.crtPEM"
-    value = tls_locally_signed_cert.mtls-intermediate-ca.cert_pem
+    value = var.mtls_intermediate_ca_cert
   }
   set {
     name  = "identity.issuer.tls.keyPEM"
-    value = tls_private_key.mtls-intermediate-ca.private_key_pem
+    value = var.mtls_intermediate_ca_private_key
   }
   set {
     name  = "identity.issuer.crtExpiry"
-    value = tls_locally_signed_cert.mtls-intermediate-ca.validity_end_time
+    value = var.mtls_cert_expiry
   }
 }
--- linkerd-helm-k8s/variables.tf
+++ linkerd-helm-k8s/variables.tf
@@ -17,3 +17,19 @@ variable "linkerd_k8s_namespace" {
   type = string
   default = "linkerd"
 }
+
+variable "mtls_root_ca_cert" {
+  type = string
+}
+
+variable "mtls_intermediate_ca_cert" {
+  type = string
+}
+
+variable "mtls_intermediate_ca_private_key" {
+  type = string
+}
+
+variable "mtls_cert_expiry" {
+  type = string
+}
